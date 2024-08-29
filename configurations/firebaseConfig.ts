60f870d043ffa68b149ee731675648bffdd0597a
Nayakyash005 <yashpankajkumarnayak@gmail.com>
2024-08-29 23:12:08 +0530

    complete

--- configurations/firebaseConfig.ts
+++ configurations/firebaseConfig.ts
@@ -1,28 +1,13 @@
-import admin, { ServiceAccount } from "firebase-admin";
+import admin from "firebase-admin";
 import path from "path";
-const firebaseconfig = {
-  type: "service_account" as string,
-  project_id: "lucid-splicer-426105-u4" as string,
-  private_key_id: process.env.PRIVATE_KEY_ID as string,
-  private_key:( process.env.PRIVATE_KEY as string).replace(/\\n/g, "\n"),
-  client_email:
-    "firebase-adminsdk-99h12@lucid-splicer-426105-u4.iam.gserviceaccount.com" as string,
-  client_id: "103529159814701260362" as string,
-  auth_uri: "https://accounts.google.com/o/oauth2/auth" as string,
-  token_uri: "https://oauth2.googleapis.com/token" as string,
-  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs" as string,
-  client_x509_cert_url:
-    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-99h12%40lucid-splicer-426105-u4.iam.gserviceaccount.com" as string,
-  universe_domain: "googleapis.com" as string,
-};
-// const serviceAccountPath = path.resolve( "./configurations/serviceAccountKey.json");
+const serviceAccountPath = path.resolve("./configurations/ServiceAccount.json");
 
-// const serviceAccount = require(serviceAccountPath);
+const serviceAccount = require(serviceAccountPath);
 admin.initializeApp({
-  credential: admin.credential.cert(firebaseconfig as ServiceAccount),
+  credential: admin.credential.cert(serviceAccount),
+
   storageBucket: "gs://lucid-splicer-426105-u4.appspot.com",
 });
-
 
 export const bucket = admin.storage().bucket();
 export default admin;

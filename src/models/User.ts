8e04be5619a19abc4e3a381fe34c6adce260b5d0
Brijendra-Singh2003 <brijendra0369@gmail.com>
2024-10-16 13:45:01 +0530

    logger

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,19 +1,6 @@
 import mongoose from "mongoose";
-// const { boolean } = require("webidl-conversions");
-// const bcrypt = require("bcrypt");
-// const jwt = require("jsonwebtoken");
+
 const userSchema = new mongoose.Schema({
-  // email: {
-  //   type: String,
-  //   required: true,
-  //   unique: true,
-  //   match: [/^\S+@\S+\.\S+$/, "Please use a valid email address."],
-  // },
-  // password: {
-  //   type: String,
-  //   required: true,
-  //   select: false,
-  // },
   firstName: {
     type: String,
     required: true,
@@ -27,50 +14,31 @@ const userSchema = new mongoose.Schema({
     unique: true,
     required: true,
   },
-  // gender: {
-  //   type: String,
-  //   enum: ["Male", "Female", ""],
-  //   required: true,
-  // },
-  // isDeteted: {
-  //   type: boolean,
-  //   enum: [true, false],
-  // },
-
-  // confirmPassword: {
-  //   type: String,
-  //   required: true,
-  //   validate: {
-  //     validator: function(value) {
-  //       return value === this.password;
-  //     },
-  //     message: 'Passwords do not match.'
-  //   }
-  // },
+  role: {
+    type: String,
+    required: true,
+    enum: ["USER", "ADMIN"],
+  },
+  department: {
+    type: String,
+    enum: [
+      "medical Assisance",
+      "Security",
+      "Handicapped Facilities",
+      "Facilities for Women with Special needs",
+      "Electrical Equipment",
+      "coach-cleanliness",
+      "Punctuality",
+      "Water Availability",
+      "coach-maintenance",
+      "Catering & Vending Services",
+      "staff behavior",
+      "Corruption/Bribery",
+      "Bed Roll"
+    ],
+  },
 });
-// userSchema.pre('save', function(next) {
-//   this.confirmPassword = undefined;
-// });
-// userSchema.pre("save", async function (next) {
-//   this.confirmPassword = undefined;
-//   if (!this.isModified("password")) {
-//     next();
-//   }
-//   this.password = await bcrypt.hash(this.password, 10);
-// });
 
-// function arrayLimit(val) {
-//   return val.length >= 1 && val.length <= 2;
-// }
-// userSchema.methods.comparePassword = async function (password) {
-//   console.log(password, this.password);
-//   return await bcrypt.compare(password, this.password);
-// };
-// userSchema.methods.getJWTToken = function () {
-//   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-//     expiresIn: process.env.JWT_EXPIRE * 24 * 60 * 60 * 1000,
-//   });
-// };
 const User = mongoose.model("User", userSchema);
 
 export default User;
